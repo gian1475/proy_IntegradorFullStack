9d83505b5fc5253ca6a4fb735aee3040e8344ce3
gian1475 <gian1475@users.noreply.github.com>
2026-09-23 00:59:54 -0500

    feat: v3

--- backend/src/test/java/com/backend/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/backend/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.backend.demo;
 
+import com.backend.demo.dao.LibroDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private LibroDao libroDao;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(libroDao);
 	}
-
 }
